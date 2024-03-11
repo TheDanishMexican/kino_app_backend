209dc1df2250a23656cb105886d834b7141b4801
TheDanishMexican <danieljchristjansen@gmail.com>
2024-03-11 09:47:07 +0100

    outcommented test

--- src/test/java/kea/kinobackend/KinobackendApplicationTests.java
+++ src/test/java/kea/kinobackend/KinobackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KinobackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
