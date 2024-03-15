d4a65336a86cc11e77a07ded9667cf3cb4fb40fc
Maltemork <malte.sorensen@protonmail.com>
2024-03-15 13:21:32 +0100

    Dont know what happened, but this should fix the users being created in the backend.

--- src/main/java/kea/kinoBackend/security/dto/UserWithRolesRequest.java
+++ src/main/java/kea/kinoBackend/security/dto/UserWithRolesRequest.java
@@ -15,4 +15,5 @@ public class UserWithRolesRequest {
     String password;
     @NonNull
     String email;
+
 }
--- src/main/java/kea/kinoBackend/security/repository/RoleRepository.java
+++ src/main/java/kea/kinoBackend/security/repository/RoleRepository.java
@@ -4,4 +4,5 @@ import kea.kinoBackend.security.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, String> {
+
 }
