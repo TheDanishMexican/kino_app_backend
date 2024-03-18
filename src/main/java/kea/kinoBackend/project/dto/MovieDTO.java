d0a94e2a3bc3a1a60157dd6a6127f6b06813570a
fili0727 <123998888+fili0727@users.noreply.github.com>
2024-03-18 11:16:02 +0100

    Update MovieDTO.java

--- src/main/java/kea/kinoBackend/project/dto/MovieDTO.java
+++ src/main/java/kea/kinoBackend/project/dto/MovieDTO.java
@@ -14,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class MovieDTO {
-    @JsonIgnore
+    
     private Integer id;
     private String name;
     private String posterUrl;
