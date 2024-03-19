01893bb6159fa0a4a484c65324961903c9f061ef
Maltemork <malte.sorensen@protonmail.com>
2024-03-19 21:49:52 +0100

    made more users

--- src/main/java/kea/kinoBackend/project/configuration/SetupTestUsers.java
+++ src/main/java/kea/kinoBackend/project/configuration/SetupTestUsers.java
@@ -44,6 +44,18 @@ public class SetupTestUsers implements ApplicationRunner {
         UserWithRoles user2 = new UserWithRoles("user2", pwEncoder.encode(passwordUsedByAll), "user2@a.dk");
         UserWithRoles user3 = new UserWithRoles("user3", pwEncoder.encode(passwordUsedByAll), "user3@a.dk");
         UserWithRoles user4 = new UserWithRoles("user4", pwEncoder.encode(passwordUsedByAll), "user4@a.dk");
+        //Create more users
+        UserWithRoles user5 = new UserWithRoles("user5", pwEncoder.encode(passwordUsedByAll), "user5@a.dk");
+        UserWithRoles user6 = new UserWithRoles("user6", pwEncoder.encode(passwordUsedByAll), "user6@a.dk");
+        UserWithRoles user7 = new UserWithRoles("user7", pwEncoder.encode(passwordUsedByAll), "user7@a.dk");
+        UserWithRoles user8 = new UserWithRoles("user8", pwEncoder.encode(passwordUsedByAll), "user8@a.dk");
+        UserWithRoles user9 = new UserWithRoles("user9", pwEncoder.encode(passwordUsedByAll), "user9@a.dk");
+        UserWithRoles user10 = new UserWithRoles("user10", pwEncoder.encode(passwordUsedByAll), "user10@a.dk");
+        UserWithRoles user11 = new UserWithRoles("user11", pwEncoder.encode(passwordUsedByAll), "user11@a.dk");
+        UserWithRoles user12 = new UserWithRoles("user12", pwEncoder.encode(passwordUsedByAll), "user12@a.dk");
+
+
+
         user1.addRole(roleUser);
         user1.addRole(roleAdmin);
         user2.addRole(roleUser);
@@ -52,6 +64,16 @@ public class SetupTestUsers implements ApplicationRunner {
         userWithRolesRepository.save(user2);
         userWithRolesRepository.save(user3);
         userWithRolesRepository.save(user4);
+        //Save more users
+        userWithRolesRepository.save(user5);
+        userWithRolesRepository.save(user6);
+        userWithRolesRepository.save(user7);
+        userWithRolesRepository.save(user8);
+        userWithRolesRepository.save(user9);
+        userWithRolesRepository.save(user10);
+        userWithRolesRepository.save(user11);
+        userWithRolesRepository.save(user12);
+
     }
 
 }
