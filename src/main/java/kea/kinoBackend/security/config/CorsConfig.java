d41c20cebc43a5605efd75b099393ffad08cdc84
TheDanishMexican <danieljchristjansen@gmail.com>
2024-03-19 14:12:01 +0100

    extended token expiration

--- src/main/java/kea/kinoBackend/security/config/CorsConfig.java
+++ src/main/java/kea/kinoBackend/security/config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig {
 
     //When you know the domain name for your client, add it below before deployment
     private static final String[] ALLOWED_ORIGINS = new String[] {
+            "https://lemon-tree-0aebf7903.5.azurestaticapps.net",
             "*",
             "https://example.com/",
             "http://127.0.0.1:5500"
